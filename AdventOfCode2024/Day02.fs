855414c059c7a53417e780a61f8b42fc43bb293b
Matt March <matthew.march@devyce.co.uk>
2024-12-02 07:50:27 +0000

    Solve day 2

---
+++ AdventOfCode2024/Day02.fs
@@ -0,0 +1,34 @@
+﻿module AdventOfCode2024.Day02
+
+open AdventOfCode2024.Common
+
+let parseLine line = line |> splitBy " " |> List.map int
+
+let differences (levels: int list) =
+    levels |> Seq.windowed 2 |> Seq.map (fun [| a; b |] -> (b - a))
+
+let isSafe (report: int list) =
+    let differences = differences report
+    let anyGreaterThan3 = differences |> Seq.map abs |> Seq.exists ((<) 3)
+    let allPositive = differences |> Seq.forall ((<) 0)
+    let allNegative = differences |> Seq.forall ((>) 0)
+    not anyGreaterThan3 && (allPositive || allNegative)
+
+let allRemovedPossibilities (report: int list) =
+    report |> Seq.mapi (fun i _ -> report |> List.removeAt i)
+
+let isSafeWithOneRemoved (report: int list) =
+    if isSafe report then
+        true
+    else
+        report |> allRemovedPossibilities |> Seq.exists isSafe
+
+let solve () =
+    let inputLines = readFile "02"
+    let safeReports = inputLines |> Seq.map parseLine |> Seq.filter isSafe |> Seq.length
+    printfn $"Day 02 - Part 1: %d{safeReports}"
+
+    let safeReportsB =
+        inputLines |> Seq.map parseLine |> Seq.filter isSafeWithOneRemoved |> Seq.length
+
+    printfn $"Day 02 - Part 2: %d{safeReportsB}"
