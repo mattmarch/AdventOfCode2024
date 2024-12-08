87051b280ec8707e12efc05e5ac521ecf356b7f5
Matt March <matthew.march@devyce.co.uk>
2024-12-08 14:11:14 +0000

    Solve day 07

--- Day07.fs
+++ Day07.fs
@@ -1,3 +1,51 @@
 ﻿module AdventOfCode2024.Day07
 
-let solve () = printfn $"Day 07 - not done"
+open AdventOfCode2024.Common
+
+let parseLine line =
+    let total, numbers = line |> splitBy ": " |> unpack2
+    int64 total, numbers |> splitBy " " |> List.map int64
+
+let isEquationPossible total numbers =
+    let rec testPossible runningTotal remainingNumbers =
+        match remainingNumbers with
+        | [] -> total = runningTotal
+        | nextNum :: rest ->
+            testPossible (runningTotal + nextNum) rest
+            || testPossible (runningTotal * nextNum) rest
+
+    match numbers with
+    | [] -> failwithf $"Empty list of numbers provided"
+    | head :: tail -> testPossible head tail
+
+let concatNums n1 n2 = (string n1 + string n2) |> int64
+
+let isEquationPossiblePart2 total numbers =
+    let rec testPossible runningTotal remainingNumbers =
+        match remainingNumbers with
+        | [] -> total = runningTotal
+        | nextNum :: rest ->
+            testPossible (runningTotal + nextNum) rest
+            || testPossible (runningTotal * nextNum) rest
+            || testPossible (concatNums runningTotal nextNum) rest
+
+    match numbers with
+    | [] -> failwithf $"Empty list of numbers provided"
+    | head :: tail -> testPossible head tail
+
+let solve () =
+    let input = readLines "07" |> Seq.map parseLine
+
+    let result =
+        input
+        |> Seq.filter (fun (total, numbers) -> isEquationPossible total numbers)
+        |> Seq.sumBy fst
+
+    printfn $"Day 07 - Part 1: %d{result}"
+
+    let result =
+        input
+        |> Seq.filter (fun (total, numbers) -> isEquationPossiblePart2 total numbers)
+        |> Seq.sumBy fst
+
+    printfn $"Day 07 - Part 2: %d{result}"
